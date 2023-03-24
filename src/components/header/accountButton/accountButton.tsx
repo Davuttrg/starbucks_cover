4b830ac914d6ab9bab5b2c5688e3c747b45edb7c
Davuttrg <davut.0773@gmail.com>
2023-03-24 11:27:27 +0300

    refactor: get account image from memo

--- src/components/header/accountButton/accountButton.tsx
+++ src/components/header/accountButton/accountButton.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAppSelector } from "../../../store/hooks";
 
@@ -5,15 +6,23 @@ export const AccountButton = () => {
   const email = useAppSelector((state) => state.user.email);
   const navigate = useNavigate();
   const handleNavigate = () => navigate("/login");
+
+  const accountImage = useMemo(
+    () => (
+      <img
+        className="rounded c-pointer"
+        width="28"
+        height="28"
+        src={`https://api.dicebear.com/5.x/initials/svg?seed=${email}`}
+      />
+    ),
+    [email]
+  );
+
   return (
     <>
       {email ? (
-        <img
-          className="rounded c-pointer"
-          width="28"
-          height="28"
-          src={`https://api.dicebear.com/5.x/initials/svg?seed=${email}`}
-        />
+        accountImage
       ) : (
         <span
           onClick={handleNavigate}
